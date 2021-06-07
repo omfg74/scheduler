dd600b759735eef4ff537ece2ecf122506eecf75
omfg <podsh74@gmail.com>
2021-06-08 00:01:41 +0500

    first

---
+++ src/main/resources/sql/changelog/v0.0/2021-06-07-initial.sql
@@ -0,0 +1,8 @@
+create table main_schdule
+(
+	id int,
+	date date,
+	boat text,
+	location text
+);
+
---
+++ src/main/resources/sql/updateLocalDb.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+java -jar \
+	libs/liquibase.jar \
+	--contexts=prod,test \
+	--driver=org.postgresql.Driver \
+	--defaultSchemaName=public \
+	--classpath=libs/postgresql-42.2.8.jar \
+	--changeLogFile=changelog/db.changelog-master.xml \
+	--url="jdbc:postgresql://localhost:5435/scheduler" \
+	--username=omfg \
+	--password=101541 \
+	update
